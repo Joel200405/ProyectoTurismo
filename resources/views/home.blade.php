a0c226e5c6a9612a8f2c5b543259e876072a48eb
steven <72620878@continental.edu.pe>
2024-10-04 17:57:07 -0500

    Se añadio los principales destinos en la pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,82 @@
 
 @section('content')
 
-    <div>
-        <h1>sjdbhnsdmg</h1>
-    </div>
+    <section class="destinos">
+        <h1>Principales Destinos</h1>
+
+        <div class="grid-container">
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="destino">
+                <img src="{{ asset('css1/img-login/homegrid.jpg') }}" alt="Roma">
+                <h2>Roma</h2>
+                <div class="info-hover">
+                    <h3>Roma</h3>
+                    <p>150 actividades</p>
+                    <p>250 000 opiniones</p>
+                    <p>8.5/10 valoracion</p>
+                </div>
+            </div>
+
+            <div class="reserve-button">
+                <button>Reservar ahora</button>
+            </div>
+
+        </div>
+
+    </section>    
+
 @endsection
