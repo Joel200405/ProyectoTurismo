972576693c6ae34bc861ed1a785d516ebff03b30
Nilton_Joel_Lazo_Maravi <74091904@continental.edu.pe>
2024-10-05 12:26:48 -0500

    Se mejoro destinos

--- resources/views/destinos/index.blade.php
+++ resources/views/destinos/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.general')
 
-<link rel="stylesheet" href="{{ asset('css/styleDestinosIndex.css') }}">
+<link rel="stylesheet" href="{{ asset('css/styleDIndex.css') }}">
 
 @section('content')
 
 <section class="destinos">
     <h1>Principales Destinos</h1>
-    <div class="container">
+    <h1>°°°°°°°°°°°°°°°°°°°°°°</h1>
         <div class="destino-display">
             <div class="destino-wrapper">
                 <div class="card">
                     <img src="{{ asset('css1/img/img2.jpg') }}" alt="Destino 1">
                     <div class="card-info">
-                        <h2>Paradise Island</h2>
-                        <p>A stunning getaway with crystal clear waters and white sand beaches.</p>
+                        <h2>Restos Arqueológicos de Colcas</h2>
+                        <p> Se encuentran a aproximadamente a 3.5 Km. al lado este de la localidad de Carhuacayán</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -21,8 +21,8 @@
                 <div class="card">
                     <img src="{{ asset('css1/img/img3.jpg') }}" alt="Destino 2">
                     <div class="card-info">
-                        <h2>Mountain Escape</h2>
-                        <p>Experience the breathtaking views and adventure in the mountains.</p>
+                        <h2>Restos Arqueológicos de Marcamarcán</h2>
+                        <p>Los restos pre incas que significa en español, PUEBLO SOBRE PUEBLOS</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -30,17 +30,17 @@
                 <div class="card">
                     <img src="{{ asset('css1/img/img4.jpg') }}" alt="Destino 3">
                     <div class="card-info">
-                        <h2>Desert Adventure</h2>
-                        <p>Explore the vastness of the desert and enjoy a unique experience.</p>
+                        <h2>Baños Termomedicinales de Collpa</h2>
+                        <p>Esta situado al oeste de Carhuacayán, a 3.5 kms, aguas de diferentes temperaturas</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
 
                 <div class="card">
-                    <img src="{{ asset('css1/img/img4.jpg') }}" alt="Destino 3">
+                    <img src="{{ asset('css1/img/img5.jpg') }}" alt="Destino 3">
                     <div class="card-info">
-                        <h2>Desert Adventure</h2>
-                        <p>Explore the vastness of the desert and enjoy a unique experience.</p>
+                        <h2>Lagunas de Hueghue y Taprash</h2>
+                        <p>Laguna situada al lado sur de la localidad, aproximadamente a 4 Km.</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -48,8 +48,8 @@
                 <div class="card">
                     <img src="{{ asset('css1/img/img2.jpg') }}" alt="Destino 1">
                     <div class="card-info">
-                        <h2>Paradise Island</h2>
-                        <p>A stunning getaway with crystal clear waters and white sand beaches.</p>
+                        <h2>Laguna de Llactamachay</h2>
+                        <p>Laguna de aguas cristalinas, cuya salida del ojo de manantial alimenta una parte a la Población</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -57,8 +57,8 @@
                 <div class="card">
                     <img src="{{ asset('css1/img/img3.jpg') }}" alt="Destino 2">
                     <div class="card-info">
-                        <h2>Mountain Escape</h2>
-                        <p>Experience the breathtaking views and adventure in the mountains.</p>
+                        <h2>Laguna de Tuctococha</h2>
+                        <p>Laguna situada a la parte occidental de la localidad de Carhuacayán, con una variedad de la fauna silvestre,</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -66,17 +66,17 @@
                 <div class="card">
                     <img src="{{ asset('css1/img/img4.jpg') }}" alt="Destino 3">
                     <div class="card-info">
-                        <h2>Desert Adventure</h2>
-                        <p>Explore the vastness of the desert and enjoy a unique experience.</p>
+                        <h2>La Bella Durmiente de Carhuacayán</h2>
+                        <p>Al lado norte de la localidad de Santa Bárbara de Carhuacayán. Se encuentran los cerros de Condorcenga</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
 
                 <div class="card">
-                    <img src="{{ asset('css1/img/img4.jpg') }}" alt="Destino 3">
+                    <img src="{{ asset('css1/img/img5.jpg') }}" alt="Destino 3">
                     <div class="card-info">
-                        <h2>Desert Adventure</h2>
-                        <p>Explore the vastness of the desert and enjoy a unique experience.</p>
+                        <h2>Laguna de Puricocha y Lucmacocha</h2>
+                        <p>Lagunas situadas al lado Nor. este de la localidad de Carhuacayan, al pie del cerro de San Antonio y Shaguag</p>
                         <a href="#" class="reserve-button">Reservar</a>
                     </div>
                 </div>
@@ -86,7 +86,6 @@
             <button class="nav-button left" onclick="moveDestinos(-1)">&#10094;</button>
             <button class="nav-button right" onclick="moveDestinos(1)">&#10095;</button>
         </div>
-    </div>
 </section>
 
 @endsection
