7f5961b82ad28dfc5e18d3b6917c83b6858dac5e
75411510 <75411510@continental.edu.pe>
2024-10-06 08:42:31 -0500

    cambio 06/10/2024

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinoController;
-<<<<<<< HEAD
 use App\Http\Controllers\homeController;
-=======
-use App\Http\Controllers\ContactController;
->>>>>>> 89a2b677c3aba8e8234e385e6c41e273efe31c29
 use App\Http\Controllers\ReservasController;
 
 
@@ -19,10 +15,6 @@ Route::get('/destinos/{id}', [DestinoController::class, 'show'])->name('destinos
 
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-<<<<<<< HEAD
-//Reservas
-=======
->>>>>>> 89a2b677c3aba8e8234e385e6c41e273efe31c29
 Route::controller(ReservasController::class)->group(function(){
     Route::get('reservas/create', 'createReserva')->name('reservas.create');
     Route::get('reservas/index', 'index')->name('reservas.index');
